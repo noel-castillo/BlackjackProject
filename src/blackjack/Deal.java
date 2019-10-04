a3f089b404eef9463c1bc6f1d2338ffcf356d921
Noel <castillo752x@gmail.com>
2019-10-04 13:43:02 -0600

    Feature: Dealer + user selects number of players

--- src/blackjack/Deal.java
+++ src/blackjack/Deal.java
@@ -4,70 +4,96 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Deal {
-	
+
 //	F I E L D S 
-	
-	List<Card> hand1;
-	List<Card> hand2;
-	
+
+	private Map<Integer, List<Card>> game;
+	private Deck deck;
+	private int playerTurn;
+	private int numberOfPlayers;
+
 //	C O N S T R U C T O R S
-	
+
 	public Deal() {
-		hand1 = new ArrayList<>();
-		hand2 = new ArrayList<>();
-		
+		deck = new Deck();
+		game = new HashMap<>();
+
 	}
-	
+
 //	M A I N 
 
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+
 		Deal deal = new Deal();
-		deal.run();
+		deal.run(kb);
+
 	}
-	
+
 //	M E T H O D S 
 
-	public void run() {
-		dealInitialHands();
+	public void run(Scanner kb) {
+		dealInitialHands(kb);
 	}
-	
-	public void dealInitialHands() {
-		Deck deck = new Deck();
+
+	public void dealInitialHands(Scanner kb) {
 		deck.createNewDeck();
 		deck.shuffle();
 
-		Map<Integer, List<Card>> game = new HashMap<>();
-		game.put(1, hand1);
-		game.put(2, hand2);
-		int playerTurn = 1;
-		for (int i = 0; i < 2; i++) {
-			Card c = deck.dealCard();
-			hand1.add(c);
-			game.put(playerTurn, hand1);
+		System.out.print("How many players? >> ");
+		numberOfPlayers = kb.nextInt();
+
+		createHands(numberOfPlayers);
+
+		for (int c = 0; c <= numberOfPlayers; c++) {
+
+			playerTurn = c;
+			for (int i = 0; i < 2; i++) {
+				Card card = deck.dealCard();
+				game.get(playerTurn).add(card);
+			}
+			printHand(game, playerTurn);
+			getValue(game, playerTurn);
 		}
-		printHandAndValue(game, playerTurn);
-		playerTurn = 2;
-		for (int i = 0; i < 2; i++) {
-			Card c = deck.dealCard();
-			hand2.add(c);
-			game.put(playerTurn, hand2);
-		}
-		printHandAndValue(game, playerTurn);
 	}
 
-	public void printHandAndValue(Map<Integer, List<Card>> game, int playerTurn) {
+	private void createHands(int numberOfPlayers) {
 
-			int value = 0;
-			for (Card card : game.get(playerTurn)) {
-				System.out.println(card);
-				value += card.getValue();
-				if (value == 21 && game.get(playerTurn).size() == 2) {
-					System.out.println("Blackjack!");
-				}
+		for (int c = 0; c <= numberOfPlayers; c++) {
+			game.put(c, new ArrayList<>());
+		}
+
+	}
+
+	public void printHand(Map<Integer, List<Card>> hand, int playerTurn) {
+
+		int value = 0;
+		if (playerTurn == 0) {
+			System.out.println("Dealer's Hand: ");
+		} else {
+			System.out.println("Player " + playerTurn + "'s Hand:");
+		}
+		for (Card card : hand.get(playerTurn)) {
+			System.out.println(card);
+			value += card.getValue();
+			if (value == 21 && hand.get(playerTurn).size() == 2) {
+				System.out.println("Blackjack!");
 			}
-			System.out.println("Player " + playerTurn + " hand value: " + value);
+		}
+	}
+
+	public int getValue(Map<Integer, List<Card>> game, int playerTurn) {
+		int value = 0;
+		for (Card card : game.get(playerTurn)) {
+			value += card.getValue();
+		}
+		System.out.println("Hand value: " + value);
+		System.out.println("===================");
+
+		return value;
 	}
 
 }
